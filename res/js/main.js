49ab437acf20c761272b471b72fe4b08751061da
Adameky <ada.david185@icloud.com>
2026-03-05 16:39:13 +0100

    prvni commit

---
+++ res/js/main.js
@@ -0,0 +1,89 @@
+//Jak propojit html s js
+//<script src="./res/js/main.js"></script>
+
+
+//Jak udelame promennou, kterou nelze menit
+
+const num = 1;
+
+//Jak udelame promennou, kterou lze menit
+
+let num1 = 1;
+
+//Jak vytvorit funkci pomoci funct
+
+
+//Jak vytvorit arrow funkci
+
+
+
+//Jak spustit funkci
+
+
+
+//Jak vytvorit funkci s parametry a, b
+
+
+
+//Jak spustit funkci a predat do parametru argumenty
+
+
+
+//Jak vratit z funkce vystup do konzole
+
+
+
+//Jak vratit z funkce vystup zpet do kodu
+
+
+ 
+
+//Jak propojit element, ktery ma id s JS
+
+
+
+//Jak vytvorit funkci, ktera se spusti pri kliknuti na tlacitko
+
+
+
+//Jak k promenne let pricist cislo 1
+
+num++;
+
+//Jak k promenne let pticist cislo 2
+
+num += 2;
+
+//Jak zmenit text elementu
+
+odstavec.innerText = "text"
+
+//Jak pridat k puvodnimu textu dalsi text
+
+odstavec.innerText += "pridany text"
+
+//Jak zmenit HTML elementu
+
+odstavec.innerHTML = "<h1>neco1</h1>"
+
+//Jak zmenit barvu textu
+
+odstavec.style.color = "red"
+
+
+//Jak vizualne odstranit element
+
+odstavec.style.display = "none"
+
+//Jak element zase vratit
+
+odstavec.style.display = "block"
+
+//Jak vytvorit uzivatelsky vstup
+
+//<input id="input" type="number">   
+
+//Jak ziskat z tohoto vstupu hodnotu
+
+const input = document.getElementById("input");
+let valueInput = input.value
